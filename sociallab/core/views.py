35b92814bc38af88466a0ab195aaad22a0830c65
johnidol <giovanni.idili@gmail.com>
2016-10-07 15:40:23 +0100

    use an html template for the index page

--- sociallab/core/views.py
+++ sociallab/core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 
 def index(request):
-    return HttpResponse("Hello - You're at the Social Lab index.")
+    template = loader.get_template('core/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
