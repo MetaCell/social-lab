071cf56f5ca3207572e5b479ec310cfcdbcf431e
johnidol <giovanni.idili@gmail.com>
2017-01-23 16:41:53 +0000

    wait page pulling instructions templates based on query string

--- _static/js/wait.js
+++ _static/js/wait.js
@@ -4,25 +4,6 @@
 $(document).ready(function(){
     var game = getParameterByName('game');
 
-    switch (game) {
-    case 'ultimatum':
-        $('#game-description').append('Ultimatum game description');
-        break;
-    case 'trust':
-        $('#game-description').append('Trust game description');
-        break;
-    case 'prisoner-dilemma':
-        $('#game-description').append('Peace-war game description');
-        break;
-    case 'chat':
-        $('#game-description').append('Chat game description');
-        break;
-    default:
-        $('#game-description').append('No game selected!');
-        $('#ready-button').hide();
-        break;
-    }
-
     $('#ready-button').click(function(){
         // TODO: open web-socket connection
         // TODO: queue up user
