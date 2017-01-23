ad2a6c9ead6b2e4a85e9ea170bd3ca4bdd2dd3e4
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2017-01-23 15:37:28 +0000

    Flipping icon

--- _static/js/games.js
+++ _static/js/games.js
@@ -1,24 +1,35 @@
 /**
  * Created by matteocantarelli on 02/12/2016.
  */
-$(document).ready(function(){
+$(document).ready(function () {
 
     //resizeGame();
 
-    window.setTimeout(function(){
+    window.setTimeout(function () {
         $('.otherplayer').popover('show');
-    },1000);
+    }, 1000);
 
-    window.setTimeout(function(){
+    window.setTimeout(function () {
         $('.self').popover('show');
-    },2000);
+    }, 2000);
+
+    window.setInterval(function () {
+        var other = $('.player.otherplayer');
+        if (other.hasClass("flip")) {
+            other.removeClass("flip");
+            other.addClass("unflip");
+        } else {
+            other.removeClass("unflip");
+            other.addClass("flip");
+        }
+    }, 5000);
 
     window.addEventListener('resize', resizeGame, false);
     window.addEventListener('orientationchange', resizeGame, false);
 
 });
 
-function showInstructions(){
+function showInstructions() {
     $(".instructionsContainer").show();
 }
 
