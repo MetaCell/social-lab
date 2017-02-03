55e2520ac5b00479a84b46c524d3b641dc2f70cf
johnidol <giovanni.idili@gmail.com>
2017-02-03 11:58:55 +0000

    some basic test to make sure the homnepage is there and as expected

--- tests/casperjs/test-vs-bot.js
+++ tests/casperjs/test-vs-bot.js
@@ -1,7 +1,7 @@
 /**
  * Created by giovanniidili on 03/02/2017.
  */
-casper.test.begin('Social Lab - Basic Test', 2, function suite(test) {
+casper.test.begin('Social Lab - Basic Test', 6, function suite(test) {
     casper.options.viewportSize = {
         width: 1340,
         height: 768
@@ -12,7 +12,11 @@ casper.test.begin('Social Lab - Basic Test', 2, function suite(test) {
         this.echo("I've waited for 10 seconds.");
         test.assertTitle("Social Lab", "Homepage title is the one expected");
         test.assertExists('div[id="socialLablogo"]', "Social lab logo found");
-      }, null, 30000);
+        test.assertExists('a[id="chat-game-link"]', "Chat game button found");
+        test.assertExists('a[id="trust-game-link"]', "Trust game button found");
+        test.assertExists('a[id="ultimatum-game-link"]', "Ultimatum game button found");
+        test.assertExists('a[id="peacewar-game-link"]', "Peace/war game button found");
+      }, null, 3000);
     });
 
     casper.run(function() {
