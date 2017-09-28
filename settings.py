fcd812559aafe30c89a48a45f768ead9251e8beb
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2017-09-28 16:58:16 +0100

    ALLOWED_HOSTS

--- settings.py
+++ settings.py
@@ -68,7 +68,7 @@ SECRET_KEY = 'oi-f81jw&)^i_n5zd$(l+*=0hg9wih23$3xzt_2$d^@#30q)zw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
